47a360c0ccdb3a87ecd31542dd907c7c31bfdfb6
5witchkr <hob4419@gmail.com>
2022-06-28 23:00:19 +0900

    feat: add MemberApp

--- springWebMvc/src/main/java/com/example/demo/MemberApp.java
+++ springWebMvc/src/main/java/com/example/demo/MemberApp.java
@@ -1,4 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MemberApp {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.signup(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
 }
