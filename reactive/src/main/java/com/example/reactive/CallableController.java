2fe0770a69dad958f144140bd05da744dc5e3a6e
5witchkr <hob4419@gmail.com>
2022-11-29 22:54:21 +0900

    feat: DeferredResult

--- reactive/src/main/java/com/example/reactive/CallableController.java
+++ reactive/src/main/java/com/example/reactive/CallableController.java
@@ -3,8 +3,11 @@ package com.example.reactive;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
+import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 @Slf4j
 @RestController
@@ -25,4 +28,27 @@ public class CallableController {
 //        Thread.sleep(2000);
 //        return "hello";
 //    }
+
+    Queue<DeferredResult<String>> results = new ConcurrentLinkedDeque<>();
+    @GetMapping("/dr")
+    public DeferredResult<String> dr() throws InterruptedException {
+        log.info("dr");
+        DeferredResult<String> dr = new DeferredResult<>();
+        results.add(dr);
+        return dr;
+    }
+
+    @GetMapping("/dr/count")
+    public String drCount(){
+        return String.valueOf(results.size());
+    }
+
+    @GetMapping("/dr/event")
+    public String drEvent(String msg) {
+        for (DeferredResult<String> dr: results) {
+            dr.setResult("Hello " + msg);
+            results.remove(dr);
+        }
+        return "OK";
+    }
 }
