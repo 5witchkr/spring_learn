284210b0a5d4f68bad9399b1eb65c1d801846f3a
5witchkr <hob4419@gmail.com>
2022-12-27 04:39:28 +0900

    CyclicBarrier, callable ramda

--- reactive/src/main/java/com/example/reactive/LoadTest.java
+++ reactive/src/main/java/com/example/reactive/LoadTest.java
@@ -4,27 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StopWatch;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class LoadTest {
     static AtomicInteger counter = new AtomicInteger(0);
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
         ExecutorService es = Executors.newFixedThreadPool(100);
 
         RestTemplate rt = new RestTemplate();
         String url = "http://localhost:8080/callable";
 
-        StopWatch main = new StopWatch();
-        main.start();
+        CyclicBarrier barrier = new CyclicBarrier(101);
 
         for (int i=0; i<100; i++) {
-            es.execute(()-> {
+            es.submit(()-> {
                 int idx = counter.addAndGet(1);
+
+                barrier.await();
+
                 log.info("Thread " + idx);
 
                 StopWatch sw = new StopWatch();
@@ -35,8 +35,16 @@ public class LoadTest {
 
                 sw.stop();
                 log.info("Elapsed: {} {}", idx, sw.getTotalTimeSeconds());
+
+                //callable ramda
+                return null;
             });
         }
+
+        barrier.await();
+        StopWatch main = new StopWatch();
+        main.start();
+
         es.shutdown();
         es.awaitTermination(100, TimeUnit.SECONDS);
 
